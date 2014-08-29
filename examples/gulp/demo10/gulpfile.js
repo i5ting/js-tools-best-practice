5a4f05927fe1145096fb5d258c9db85a50158fd6
kezhizhi <kezhizhi@163.com>
2014-08-29 16:10:24 +0800

    connect demo

--- examples/gulp/demo10/gulpfile.js
+++ examples/gulp/demo10/gulpfile.js
@@ -1,16 +1,22 @@
+//引入插件
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 
-gulp.task('watch', function() {
-	gulp.watch(['../../../src/*.html'],['html'])
+//创建watch任务去检测文件的改动
+gulp.task('watch', function () {
+    gulp.watch(['../../../src/*.html'], ['html']);
 });
-gulp.task('connect',function(){
-	connect.server({
-		root:'aaa',
-		livereload:true
-	})
-})
-gulp.task('html',function(){
-	gulp.src('../../../src/*.html').pipe(connect.reload());
-})
-gulp.task('default',['connect','watch'])
+//使用connect启动一个Web服务器
+gulp.task('connect', function () {
+    connect.server({
+        root: '../../../src',
+        livereload: true
+    });
+});
+//调用要执行的任务
+gulp.task('html', function () {
+    gulp.src('../../../src/*.html').pipe(connect.reload());
+});
+
+//运行任务
+gulp.task('default', ['connect', 'watch']);
